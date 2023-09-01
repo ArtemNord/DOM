759be9420411ff5618b824201aa56f625a4c790e
Nord.A <mr.nordakov@gmail.com>
2023-09-01 15:46:11 +0300

    lesson2.v2

--- lessons/lesson2/script.js
+++ lessons/lesson2/script.js
@@ -10,3 +10,25 @@ imgEl.setAttribute("alt", "cool img");
 
 content.appendChild(h1El);
 content.appendChild(imgEl);
+
+//--------------------------------------------
+
+const liEl = document.querySelectorAll(".menu__list");
+const liElOne = document.querySelector(".menu__list");
+const linkEl = document.querySelector(".menu__link");
+const menuEl = document.querySelector(".menu");
+
+// console.log(liEl);
+// console.log(liElOne.parentNode.children);
+
+// console.log(linkEl.children);
+// console.log(linkEl.childNodes);
+
+// console.log(menuEl.children);
+// console.log(menuEl.childNodes);
+
+menuEl.childNodes.forEach((element) => {
+  console.log(element);
+});
+
+// Spred, rest operators
