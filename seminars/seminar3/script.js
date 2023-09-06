1cd4424fc9c3283f7ab4ac536d40c341e2c8ac80
Naganishe <mr.nordakov@mail.ru>
2023-09-07 01:01:24 +0300

    lessons&seminars

---
+++ seminars/seminar3/script.js
@@ -0,0 +1,17 @@
+//task1
+
+document.querySelector(".myButton").onclick = function () {
+  console.log("Событие онклик");
+};
+
+//-----------------------
+
+window.onload = function () {
+  console.log("Страница прогрузилась");
+};
+
+window.addEventListener("load", () => {
+  console.log("Страница прогружена");
+});
+
+//task2
