32d8c83f7719d56a5206578b1a5eb9e33357afc0
Naganishe <mr.nordakov@mail.ru>
2023-09-13 22:51:41 +0300

    seminar4.v2

--- seminars/seminar4/script.js
+++ seminars/seminar4/script.js
@@ -1,84 +1,84 @@
-const checkbox = document.querySelector("#agree");
-const submitBtn = document.querySelector(".button");
-const body = document.querySelector("body");
-const div = document.querySelector(".message");
+// const checkbox = document.querySelector("#agree");
+// const submitBtn = document.querySelector(".button");
+// const body = document.querySelector("body");
+// const div = document.querySelector(".message");
 
-submitBtn.addEventListener("click", clickTheButton);
+// submitBtn.addEventListener("click", clickTheButton);
 
-function clickTheButton() {
-  if (!checkbox.checked) {
-    div.textContent = "Необходимо согласиться с условием";
-    div.style.color = "red";
-  } else {
-    div.textContent = "Согласились";
-    div.style.color = "green";
-  }
-}
+// function clickTheButton() {
+//   if (!checkbox.checked) {
+//     div.textContent = "Необходимо согласиться с условием";
+//     div.style.color = "red";
+//   } else {
+//     div.textContent = "Согласились";
+//     div.style.color = "green";
+//   }
+// }
 
-//-------------------------------------
+// //-------------------------------------
 
-const teaBtn = document.querySelector("input[value='tea'");
-const coffeeBtn = document.querySelector("input[value='coffee']");
-const drinkBtn = document.querySelector(".drink-button");
+// const teaBtn = document.querySelector("input[value='tea'");
+// const coffeeBtn = document.querySelector("input[value='coffee']");
+// const drinkBtn = document.querySelector(".drink-button");
 
-drinkBtn.addEventListener("click", () => {
-  if (teaBtn.checked) {
-    alert("Чай закончился");
-  } else if (coffeeBtn.checked) {
-    alert("Coffee закончился");
-  }
-});
+// drinkBtn.addEventListener("click", () => {
+//   if (teaBtn.checked) {
+//     alert("Чай закончился");
+//   } else if (coffeeBtn.checked) {
+//     alert("Coffee закончился");
+//   }
+// });
 
-//---------------------------------------
+// //---------------------------------------
 
-const form = document.querySelector(".form");
-const passwordField = document.querySelector(".password");
-const submitButton = document.querySelector(".pass-button");
-const togglePasswordButton = document.querySelector(".toggle-password");
-togglePasswordButton.style.display = "none";
-form.style.margin = "150px";
+// const form = document.querySelector(".form");
+// const passwordField = document.querySelector(".password");
+// const submitButton = document.querySelector(".pass-button");
+// const togglePasswordButton = document.querySelector(".toggle-password");
+// togglePasswordButton.style.display = "none";
+// form.style.margin = "150px";
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (passwordField.value === "пароль") {
-    passwordField.style.borderColor = "green";
-    passwordField.style.borderWidth = "3px";
-    passwordField.setCustomValidity("Пароль верный");
-  } else {
-    passwordField.style.borderColor = "red";
-    passwordField.style.borderWidth = "3px";
-    passwordField.setCustomValidity("Пароль не верный");
-  }
-  passwordField.reportValidity();
-});
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   if (passwordField.value === "пароль") {
+//     passwordField.style.borderColor = "green";
+//     passwordField.style.borderWidth = "3px";
+//     passwordField.setCustomValidity("Пароль верный");
+//   } else {
+//     passwordField.style.borderColor = "red";
+//     passwordField.style.borderWidth = "3px";
+//     passwordField.setCustomValidity("Пароль не верный");
+//   }
+//   passwordField.reportValidity();
+// });
 
-passwordField.addEventListener("input", (event) => {
-  passwordField.setCustomValidity("");
-});
+// passwordField.addEventListener("input", (event) => {
+//   passwordField.setCustomValidity("");
+// });
 
-passwordField.addEventListener("input", () => {
-  if (passwordField.value === "") {
-    togglePasswordButton.style.display = "none";
-  } else {
-    togglePasswordButton.style.display = "inline-block";
-  }
-});
+// passwordField.addEventListener("input", () => {
+//   if (passwordField.value === "") {
+//     togglePasswordButton.style.display = "none";
+//   } else {
+//     togglePasswordButton.style.display = "inline-block";
+//   }
+// });
 
-togglePasswordButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const type =
-    passwordField.getAttribute("type") === "password" ? "text" : "password";
-  passwordField.setAttribute("type", type);
-  togglePasswordButton.textContent =
-    type === "password" ? "Показать пароль" : "Скрыть пароль";
-});
+// togglePasswordButton.addEventListener("click", (event) => {
+//   event.preventDefault();
+//   const type =
+//     passwordField.getAttribute("type") === "password" ? "text" : "password";
+//   passwordField.setAttribute("type", type);
+//   togglePasswordButton.textContent =
+//     type === "password" ? "Показать пароль" : "Скрыть пароль";
+// });
 
 //----------------------------------
 
-textInput.addEventListener("input", changeInput);
-
-const changeInput = () => {
-  const textInput = document.querySelector(".text-input");
+const changeHeader = () => {
+  const input = document.querySelector("#input");
   const header = document.querySelector("h1");
-  header.innerText = textInput.value;
+  header.innerText = input.value;
 };
+
+input.addEventListener("input", changeHeader);
